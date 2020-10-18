08f25fd2f4b16e31cc5db6330b1eab2e4bfc39fd
Moses Ngwerume <msngwelz@gmail.com>
2020-10-18 22:50:56 +0200

    Go to the next page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Learn to design and code React Apps</h1>
+    <p>I am going to be a great Front-End developer and companies are going 
+      to hunt me down.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/page-2/">Go to the second page</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
